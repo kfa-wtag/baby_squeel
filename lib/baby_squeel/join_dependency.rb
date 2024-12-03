1da679ca9a71d43640c986c5d543c2fdfef31e5d
kfa-wtag <khondakar.fahad@welldev.io>
2024-12-03 13:51:09 +0600

    chore: upgrade to rails 7.2.2

--- lib/baby_squeel/join_dependency.rb
+++ lib/baby_squeel/join_dependency.rb
@@ -119,7 +119,7 @@ module BabySqueel
 
         join_list = join_nodes + joins
 
-        alias_tracker = Associations::AliasTracker.create(relation.klass.connection, relation.table.name, join_list)
+        alias_tracker = relation.alias_tracker(join_list)
         join_dependency = Associations::JoinDependency.new(relation.klass, relation.table, association_joins, Arel::Nodes::InnerJoin)
         join_dependency.instance_variable_set(:@alias_tracker, alias_tracker)
 
